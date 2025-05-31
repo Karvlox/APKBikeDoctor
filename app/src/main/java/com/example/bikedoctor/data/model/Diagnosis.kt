25a0499bed415c7cecf7c538a44e74007fb8b401
karvlox <135563580+Karvlox@users.noreply.github.com>
2025-05-31 00:57:58 -0400

    feat: Solve Problems

--- app/src/main/java/com/example/bikedoctor/data/model/Diagnosis.kt
+++ app/src/main/java/com/example/bikedoctor/data/model/Diagnosis.kt
@@ -9,7 +9,7 @@ data class Diagnosis(
     val clientCI: Int?,
     val motorcycleLicensePlate: String?,
     val employeeCI: Int?,
-    val listDiagnostic: List<Diagnostic>?,
+    val listDiagnostic: List<Diagnostic>? = emptyList(),
     val reviewed: Boolean?
 )
 
@@ -18,7 +18,7 @@ data class DiagnosisPost(
     val clientCI: Int?,
     val motorcycleLicensePlate: String?,
     val employeeCI: Int?,
-    val listDiagnostic: List<Diagnostic>? = emptyList(),
+    val listDiagnostic: List<Diagnostic>?,
     val reviewed: Boolean? = false
 )
 
