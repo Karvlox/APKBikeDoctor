1f80a47eeed13feb3b24a60267680eb52e91913b
karvlox <135563580+Karvlox@users.noreply.github.com>
2025-07-08 12:31:47 -0400

    feat: Added the final part of profile

--- app/src/main/java/com/example/bikedoctor/data/remote/RetrofitStaff.kt
+++ app/src/main/java/com/example/bikedoctor/data/remote/RetrofitStaff.kt
@@ -4,9 +4,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitStaff {
-    private const val BASE_URL = "https://authenticationservice-production-6068.up.railway.app/"
+    private const val BASE_URL = "https://authenticationservice-production-df3c.up.railway.app/"
 
     //http://authenticationservice-production-6068.up.railway.app/
+    //https://authenticationservice-production-df3c.up.railway.app/
+
     val staffApi: StaffApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
